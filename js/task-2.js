41c2a3f9bde03fa1fcb861ab0411b62fa4bc7719
Sergkras <72658674+Sergkras@users.noreply.github.com>
2021-02-28 17:17:15 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,17 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы'
+];
+const ingredientsRef = document.querySelector('#ingredients');
 
+const arrayRef = ingredients.map(elem => {
+    const creatliRef = document.createElement('li');
+    creatliRef.textContent = elem;
+    return creatliRef;
+});
+
+ingredientsRef.append(...arrayRef);
