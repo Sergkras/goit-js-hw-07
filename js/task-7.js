1ec76c19db0ecabb5c6a1a792bb4becadb5aa333
Sergkras <72658674+Sergkras@users.noreply.github.com>
2021-02-28 17:20:44 +0200

    Update task-7.js

--- js/task-7.js
+++ js/task-7.js
@@ -1,1 +1,8 @@
+const sizeFontRef = document.querySelector("#font-size-control");
+const textRef = document.querySelector("#text");
 
+sizeFontRef.addEventListener("input", chengSizeRef);
+
+function chengSizeRef(event) {
+    textRef.style.fontSize = `${event.currentTarget.value}px`;
+};
