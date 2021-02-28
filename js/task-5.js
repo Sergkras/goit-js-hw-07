c21b2d22873dba0d59e1342333bba70757fb60f5
Sergkras <72658674+Sergkras@users.noreply.github.com>
2021-02-28 17:19:29 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -1,1 +1,8 @@
+const holderRef = document.querySelector('#name-input');
+const textOutRef = document.querySelector('#name-output');
 
+holderRef.addEventListener('input', onAddText);
+
+function onAddText(event) {
+    textOutRef.textContent = (event.currentTarget.value === "") ? 'незнакомец' : event.currentTarget.value;
+};
