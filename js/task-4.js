d2fdbfb5de86cd4c8d09c4a8c31bf446df239c2e
Sergkras <72658674+Sergkras@users.noreply.github.com>
2021-02-28 17:18:47 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,14 @@
+const counterValue = document.querySelector('#value');
 
+const counterRef = document.querySelector('#counter');
+const onBtnDec = document.querySelector('button[data-action="decrement"]');
+const onBtnInc = document.querySelector('button[data-action="increment"]');
+
+const addNumber = () => {
+    counterValue.textContent = Number(counterValue.textContent) + 1;
+};
+const removeNumber = () => {
+    counterValue.textContent = Number(counterValue.textContent) - 1;
+};
+onBtnDec.addEventListener('click', removeNumber);
+onBtnInc.addEventListener('click', addNumber);
