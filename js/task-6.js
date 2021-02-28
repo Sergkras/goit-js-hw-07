98b34bbd0aa57659278e39a0e926a5935a18c28e
Sergkras <72658674+Sergkras@users.noreply.github.com>
2021-02-28 17:20:10 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,1 +1,13 @@
+const inputRef = document.querySelector("#validation-input");
 
+function blurFocus(event) {
+    event.preventDefault();
+    if (
+        `${event.currentTarget.value.length}` ===
+        `${inputRef.getAttribute("data-length")}`
+    ) {
+        return inputRef.setAttribute("class", "valid");
+    }
+    return inputRef.setAttribute("class", "invalid");
+}
+inputRef.addEventListener("blur", blurFocus);
